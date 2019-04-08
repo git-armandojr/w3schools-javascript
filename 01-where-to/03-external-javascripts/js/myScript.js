cfff1ee07844e4445c8d004cd8f255bb053e50ad
Armando <mail.armando@gmail.com>
2019-04-08 16:52:34 -0300

    JS Where To

---
+++ 01-where-to/03-external-javascripts/js/myScript.js
@@ -0,0 +1,3 @@
+function myFunctionInFolder() {
+    document.getElementById("external-folder").innerHTML = "Texto escrito com uma função chamada de um arquivo JavaScript externo dentro de uma pasta.";
+}
---
+++ 01-where-to/03-external-javascripts/myScript.js
@@ -0,0 +1,3 @@
+function myFunctionInFile() {
+    document.getElementById("external-file").innerHTML = "Texto escrito com uma função chamada de um arquivo JavaScript externo na raiz do site.";
+}
